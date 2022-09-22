db0463bc6e9386e5a23895357325f0c368762be6
carmenlancho <carmenlancho@hotmail.com>
2022-09-22 12:40:02 +0200

    pruebas

--- prueba.R
+++ prueba.R
@@ -1,1 +1,8 @@
 # prueba2d9ddddddddddddddd
+install.packages('credentials')
+install.packages('gitcreds')
+library(credentials)
+library(gitcreds)
+
+credentials::set_github_pat()
+gitcreds::gitcreds_set()
